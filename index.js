2d596fe965591bbb20613539e7cb08a4ea146f94
retsnimle <retsnimle@gmail.com>
2017-08-27 22:00:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -55,16 +55,65 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+//測試由此開始
+function replyToLine(rplyToken, rplyVal, rplyType, value1, value2) {
+
+  if (rplyType==text){
+    let rplyObj = {
+     replyToken: rplyToken,
+     messages: [
+        {
+         type: "text",
+         text: rplyVal
+         //type: "sticker",
+         //packageId: "1",
+         //stickerId: "1"
+       }
+     ]
+    }
+   }
+  
+  if (rplyType==sticker){
+    let rplyObj = {
+     replyToken: rplyToken,
+     messages: [
+        {
+         //type: "text",
+         //text: rplyVal
+         type: "sticker",
+         packageId: value1,
+         stickerId: value2
+       }
+     ]
+    }
+   }
+
+  let rplyJson = JSON.stringify(rplyObj); 
+  
+  var request = https.request(options, function(response) {
+    console.log('Status: ' + response.statusCode);
+    console.log('Headers: ' + JSON.stringify(response.headers));
+    response.setEncoding('utf8');
+    response.on('data', function(body) {
+      console.log(body); 
+    });
+  });
+  request.on('error', function(e) {
+    console.log('Request error: ' + e.message);
+  })
+  request.end(rplyJson);
+}
+
 function replyMsgToLine(rplyToken, rplyVal) {
   let rplyObj = {
     replyToken: rplyToken,
     messages: [
       {
-        //type: "text",
-        //text: rplyVal
-        type: "sticker",
-        packageId: "1",
-        stickerId: "1"
+        type: "text",
+        text: rplyVal
+        //type: "sticker",
+        //packageId: "1",
+        //stickerId: "1"
       }
     ]
   }
@@ -86,45 +135,34 @@ function replyMsgToLine(rplyToken, rplyVal) {
 }
 
 function parseInput(rplyToken, inputStr) {
-  console.log('InputStr: ' + inputStr);
-  let msgSplitor = ' ';
-  let comSplitor = 'd';
-
-  let mainMsg = inputStr.split(msgSplitor);
-  let trigger = mainMsg[0];
-  console.log(trigger);
-  if (trigger != '李孟儒') return null;
-
-  _isNaN = function(obj) {
-    return isNaN(parseInt(obj));
-  }
-
-  let commandArr = mainMsg[1].split(comSplitor);
-  if (commandArr.length != 2 || _isNaN(commandArr[0]) || _isNaN(commandArr[1])) return randomReply();
-  let countOfNum = commandArr[0];
-  let randomRange = commandArr[1];
-  
-  let countStr = '';
-  let count = 0;
-  for (let idx = 1; idx <= countOfNum; idx ++) {
-    let temp = random(1, randomRange);
-    countStr = countStr + temp + '+';
-    count += temp; 
-  }
-  
-  if (countOfNum == 1) {
-    countStr = count;
-  } else {
-    countStr = countStr.substring(0, countStr.length - 1) + '=' + count;
-  }
-  return countStr;
-}
-
-function random(min, max) {
-  return Math.floor((Math.random() * max) + min);
-}
-
-function randomReply() {
-  let rplyArr = ['幹你娘不要亂打好嗎？懂？', '你媽知道你在這裡發廢文嗎？', '啊啊啊啊～我要被玩壞惹', '幫QQ', '我看不到明天的陽光', '在非洲，每六十秒，就有一分鐘過去。認同請分享', '只要每天省下買一杯奶茶的錢，十天後就能買十杯奶茶', '當你的左臉被人打，那你的左臉就會痛', '誰能想的到，這名16歲少女，在四年前，只是一名12歲少女', '當蝴蝶在南半球拍了兩下翅膀，牠就會稍微飛高一點點', '台灣競爭力低落，在美國就連小學生都會說流利的英語', '每呼吸60秒，就減少一分鐘的壽命', '據統計，未婚生子的人數中有高機率為女性'];
-  return rplyArr[random((rplyArr.length - 1), 0)];
-}
+        console.log('InputStr: ' + inputStr);
+        _isNaN = function(obj) {
+         return isNaN(parseInt(obj));
+        }
+        if (inputStr.match('test') != null) return "Good" ;
+        else 
+        if (inputStr.match('sticker') != null) {
+          let rplyArr = inputStr.split(' ');
+          replyToLine(rplyToken, 0, sticker, rplyArr[1], rplyArr[2]);
+          return undefined;        
+        }
+        
+     //cc判定在此
+     //   if (inputStr.toLowerCase().match(/^cc/)!= null) return CoC7th(inputStr.toLowerCase()) ;      
+     //   else
+        //入幫測驗判定在此
+     //   if (inputStr.match('鴨霸幫入幫測驗') != null) return Yababang(inputStr) ;      
+     //   else
+        //pbta判定在此
+     //   if (inputStr.toLowerCase().match(/^pb/)!= null) return pbta(inputStr.toLowerCase()) ;      
+     //   else
+        //擲骰判定在此        
+     //   if (inputStr.match(/\w/)!=null && inputStr.toLowerCase().match(/d/)!=null) {
+     //     return nomalDiceRoller(inputStr);
+        }
+     //   else
+        //鴨霸獸指令開始於此
+     //   if (inputStr.match('鴨霸獸') != null) return YabasoReply(inputStr) ;
+        else return undefined;
+        
+      }
