905cc56ec5be65846a958b6e5f24607540000e7b
retsnimle <retsnimle@gmail.com>
2017-08-27 22:12:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -87,12 +87,42 @@ function replyMsgToLine(rplyToken, rplyVal) {
   request.end(rplyJson);
 }
 
+function replyStkToLine(rplyToken) {
+  let rplyObj = {
+    replyToken: rplyToken,
+    messages: [
+      {
+        //type: "text",
+        //text: rplyVal
+        type: "sticker",
+        packageId: "1",
+        stickerId: "1"
+      }
+    ]
+  }
+
+  let rplyJson = JSON.stringify(rplyObj); 
+  
+  var request = https.request(options, function(response) {
+    console.log('Status: ' + response.statusCode);
+    console.log('Headers: ' + JSON.stringify(response.headers));
+    response.setEncoding('utf8');
+    response.on('data', function(body) {
+      console.log(body); 
+    });
+  });
+  request.on('error', function(e) {
+    console.log('Request error: ' + e.message);
+  })
+  request.end(rplyJson);
+}
+
 function parseInput(rplyToken, inputStr) {
         console.log('InputStr: ' + inputStr);
         _isNaN = function(obj) {
          return isNaN(parseInt(obj));
         }
-        replyMsgToLine(rplyToken, inputStr);
+        replyStkToLine(rplyToken);
   
         //if (inputStr.match('test') != null) return "Good" ;
      //   else 
